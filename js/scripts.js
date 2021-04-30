c6a3cddde098d864a1cbe15d7036af73623dc6a6
RGB Studios <justingolden21@gmail.com>
2021-04-29 22:55:56 -0700

    track dice rolled and minor UI additions

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,29 @@
+let diceRolled;
+
 window.onload = ()=> {
 	u('#start-btn').on('click', ()=> {
+		diceRolled = 0;
 		const aimFor = validate('aim-for', 20);
 		const numSimulations = validate('num-simulations', 10000);
 
 		const result = simulateMany(aimFor, numSimulations);
-
 		u('#output').html(`Aimed for <b>${aimFor}</b> and performed <b>${numSimulations}</b> trials.
 			<br>Total: <b>${result}</b>
-			<br>Average: <b>${result/numSimulations}</b>`);
+			<br>Average: <b>${result/numSimulations}</b>
+			<br><small>(rolled ${diceRolled} dice)</small>`);
 	});
 
 	u('#start-all-btn').on('click', ()=> {
+		diceRolled = 0;
 		const numSimulations = validate('num-simulations', 10000);
 
-		let results = [];
 		let output = `Performed <b>${numSimulations}</b> trials.<br>`;
-		for(let aimFor=2; aimFor<50; aimFor++) {
+		for(let aimFor=2; aimFor<=50; aimFor++) {
 			const result = simulateMany(aimFor, numSimulations);
-			results.push(result);
 			output += `Aimed for <b>${aimFor}</b> and averaged <b>${result/numSimulations}</b> <div style="background: #0D47A1; height: 8px; width: ${result/numSimulations*100}px"></div>`;
 		}
 
-		u('#output').html(output);
+		u('#output').html(output + `<br><small>(rolled ${diceRolled} dice)</small>`);
 	});
 };
 
@@ -34,11 +36,11 @@ function simulateMany(aimFor, numSimulations) {
 }
 
 function simulate(aimFor) {
-	let roll = randomInt(1,6);
-	let total = roll;
-	while(roll != 1 && total < aimFor) {
+	let total = 0, roll;
+	do {
 		roll = randomInt(1,6);
 		total += roll;
-	}
+		diceRolled++;
+	} while(roll != 1 && total < aimFor);
 	return roll==1 ? 0 : total;
 }
